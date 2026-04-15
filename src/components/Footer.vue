f81a1ae20b946c21b62c0acc696ff10e52d49141
Zhanasyl08 <iamzhanasyl.omar@gmail.com>
2026-04-15 17:34:01 +0500

    commit

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -8,55 +8,56 @@
       <div class="footer_body">
         <div class="footer_feedback">
           <div class="footer_feedback__item">
-            <div class="footer_feedback__item_city">Казахстан, г. Астана</div>
+            <div class="footer_feedback__item_city">Казахстан, все города</div>
             <div class="footer_feedback__item_contact">Контакты:</div>
-            <div class="footer_feedback__item_tel">Тел: +7 707 170 7036</div>
-            <div class="footer_feedback__item_mail">
-              Email: asiqazaqstan@gmail.com
-            </div>
+            <div class="footer_feedback__item_tel">Тел: +7 777 777 7777</div>
+            <div class="footer_feedback__item_mail">Email: example@mail.ru</div>
           </div>
           <div class="footer_feedback__item">
-            <div class="footer_feedback__item_list">О форуме</div>
-            <div class="footer_feedback__item_list">Тарифы</div>
-            <div class="footer_feedback__item_list">Организаторы</div>
-            <div class="footer_feedback__item_list">Контакты</div>
+            <div class="footer_feedback__item_list">Лицензия</div>
+            <div class="footer_feedback__item_list">Правила</div>
+            <div class="footer_feedback__item_list">Возврат и обмен</div>
+            <div class="footer_feedback__item_list">Доставка</div>
+            <div class="footer_feedback__item_list">Гарантия</div>
+            <div class="footer_feedback__item_list">Точки</div>
+            <div class="footer_feedback__item_list">Оптом</div>
+            <div class="footer_feedback__item_list">Розница</div>
             <div class="footer_feedback__item_social">
               <div class="footer_feedback__item_social_icon">
-                <a href="ссылка"><i class="fab fa-instagram"></i></a>
+                <a href="ссылка"><i class="fa-brands fa-instagram"></i></a>
               </div>
               <div class="footer_feedback__item_social_icon">
-                <a href="ссылка"><i class="fab fa-youtube"></i></a>
+                <a href="ссылка"><i class="fa-brands fa-youtube"></i></a>
               </div>
               <div class="footer_feedback__item_social_icon">
-                <a href="ссылка"><i class="fab fa-facebook"></i></a>
+                <a href="ссылка"><i class="fa-brands fa-facebook"></i></a>
               </div>
               <div class="footer_feedback__item_social_icon">
-                <a href="ссылка"><i class="fab fa-whatsapp"></i></a>
+                <a href="ссылка"><i class="fa-brands fa-whatsapp"></i></a>
               </div>
             </div>
           </div>
           <div class="footer_feedback__item">
             <div class="footer_feedback__item_img">
-              <img src="./img/footer_logo.svg" alt="logo" />
+              <img src="../assets/img/2.png" alt="logo" />
             </div>
           </div>
           <div class="footer_feedback__item">
             <div class="footer_feedback__item_text">
-              Евразийский Құрылтай — это центр идей и возможностей для развития
-              социального бизнеса и устойчивого будущего.
+              Наш бренд - молодежный, современный, минималистичный выбор каждого
+              человека. Мы даем 100% гарантию на возврат и обмен при наличии
+              дефектов , брака в товаре. Так же в течении недели вы можете
+              сделать обмен если Размер не подошел.
             </div>
             <div class="footer_feedback__item_text">
-              Евразийский Құрылтай социальных предпринимателей — это крупнейшая
-              международная площадка для обмена опытом, обсуждения инноваций и
-              создания партнерств в области социального бизнеса и устойчивого
-              развития.<br />
+              Born in 777. Во времена Атлахской битвы мы основались и изначаль
+              делали одежду для батыров ханов и всем кому не лень. Чингизхан
+              один из наших звезд которые доверяют нам.<br />
             </div>
             <div class="footer_feedback__item_text">
-              Евразийский Құрылтай социальных предпринимателей — это уникальная
-              возможность для лидеров социального бизнеса из 20 стран
-              объединиться, чтобы вдохновляться новыми идеями, находить решения
-              глобальных вызовов и расширять границы социального
-              предпринимательства.
+              Grid - это уникальная возможность выделиться, В этом мире где все
+              соблюдают какой либо стандарт в одежде, наш бренд полный антоним
+              этого ведь Випы Нефоры Абушки все необычные в нашей одежде.
             </div>
           </div>
         </div>
--- src/main.js
+++ src/main.js
@@ -2,5 +2,5 @@ import { createApp } from "vue";
 import "./style/style.scss";
 import App from "./App.vue";
 import router from "./router";
-
+import "@fortawesome/fontawesome-free/css/all.css";
 createApp(App).use(router).mount("#app");
